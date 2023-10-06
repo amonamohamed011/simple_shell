6d6efc3159d3075444d97630e39bbadb98d921f1
amonamohamed011 <amonamohamed011@gmail.com>
2023-10-06 12:56:00 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void amnaandruba_print(const char *letter);
+void proceed_order(const char *order);
+void show_prompt(void);
+void learn_order(char *order, size_t size);
+
+#endif
