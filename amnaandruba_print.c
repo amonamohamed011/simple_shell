471e771cfdeb983fa53836bc790bc43dbebd34c5
Ruba124 <r.m.d.abkam@gmail.com>
2023-10-06 23:22:50 +0300

    2 files done

---
+++ amnaandruba_print.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+/**
+ * amnaandruba_print - function
+ * @letter:the string to be printed
+ * Return:This function doesnot return any value
+ */
+void amnaandruba_print(const char *letter)
+{
+	int r = 1;
+
+	while (r = 1)
+	{
+		write(1, &letter, strlen(letter))
+	}
+}
