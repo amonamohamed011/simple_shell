3f3699f2b660d44891fe1488e91f51271af27b9e
amonamohamed011 <amonamohamed011@gmail.com>
2023-10-06 15:53:13 -0700

    execute.c

--- execute.c
+++ execute.c
@@ -2,16 +2,16 @@
 void proceed_order(const char *order)
 {
 pid_t child_pid = fork();
-if (child_pid == -1)
-{
-perror("fork error");
-return 1;
-}
-else if (child_pid == 0)
+if (child_pid == 0)
 {
 printf("this is child process (PID : %d)\n", getpid());
 execlp(order, order, (char *)NULL);
 perror(execlp);
+return 1;
+}
+else if (child_pid == -1)
+{
+perror("fork error");
 return 1;
 }
 else
