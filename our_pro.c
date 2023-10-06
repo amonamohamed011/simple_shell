1035da073372cbe6583ffb379be87b710a41542a
Ruba124 <r.m.d.abkam@gmail.com>
2023-10-07 00:03:50 +0300

    3 done

---
+++ our_pro.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+/**
+ * show_prompt - function
+ * Return:no return value
+ */
+void show_prompt(void)
+{
+	int a = 7;
+
+	//just creating what we are going to see in every command
+	while (a > 0)
+	{
+		amnaandruba_print("$");
+	}
+}
