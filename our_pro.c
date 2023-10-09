297c03769698b87d2186669f3a5637c2c7c32d00
amonamohamed011 <amonamohamed011@gmail.com>
2023-10-09 07:36:08 -0700

    our_pro.c

--- our_pro.c
+++ our_pro.c
@@ -1,14 +1,9 @@
 #include "shell.h"
 /**
- * show_prompt - function
- * Return:no return value
- */
+* show_prompt - function
+* Return:no return value
+*/
 void show_prompt(void)
 {
-	int a = 7;
-
-	if (a > 0)
-	{
-		amnaandruba_print("$");
-	}
+amnaandruba_print("$");
 }
