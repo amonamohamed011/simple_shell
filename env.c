581ab434ab16e45a21962d861f2f88ce332b234c
amonamohamed011 <amonamohamed011@gmail.com>
2023-10-18 09:53:29 -0700

    env.c

--- env.c
+++ env.c
@@ -1,37 +1,36 @@
 #include "shell.h"
 /**
- * RA_env - env custom function
- */
+* RA_env - this is env custom function
+*/
 void RA_env(void)
 {
-	char **env = environ;
+char **env = environ;
 
-	while (*env)
-	{
-		amnaandruba_print(*env);
-		amnaandruba_print("\n");
-		env++;
-	}
+while (*env)
+{
+amnaandruba_print(*env);
+amnaandruba_print("\n");
+env++;
+}
 }
 /**
- * RA_getenv - our custom function
- * @name:the parameter
- * Return:depends
- */
+* RA_getenv - our custom function
+* @name:the parameter
+* Return:depends
+*/
 char *RA_getenv(const char *name)
 {
-	char *RA_var;
-	int len = strlen(name);
-	char **AR_env;
+char *RA_var;
+int len = strlen(name);
+char **AR_env;
 
-	for (AR_env = environ; *AR_env != NULL; AR_env++)
-	{
-		RA_var = *AR_env;
-		if (strncmp(name, RA_var, len) == 0 && RA_var[len] == '=')
-		{
-			return (&RA_var[len + 1]);
-		}
-	}
-	return (NULL);
+for (AR_env = environ; *AR_env != NULL; AR_env++)
+{
+RA_var = *AR_env;
+if (strncmp(name, RA_var, len) == 0 && RA_var[len] == '=')
+{
+return (&RA_var[len + 1]);
 }
-
+}
+return (NULL);
+}
