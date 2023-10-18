037be78884de6f66ebf9c300871eaddea14960dd
amonamohamed011 <amonamohamed011@gmail.com>
2023-10-18 08:23:50 -0700

    cd.c

--- cd.c
+++ cd.c
@@ -1,21 +1,20 @@
 #include "shell.h"
 /**
- * RA_cd - function
- * @com:para
+ * RA_cd - this is a function.
+ * @com: parameter.
  */
 void RA_cd(char **com)
 {
-	if (!com[1] || RA_strcmp(com[1], "-") == 0)
-	{
-		chdir(RA_getenv("HOME"));
-	}
-	else
-	{
-		if ((chdir(com[1]) != 0))
-		{
-			free(com);
-			perror("cd");
-		}
-	}
+if (!com[1] || RA_strcmp(com[1], "-") == 0)
+{
+chdir(RA_getenv("HOME"));
 }
-
+else
+{
+if ((chdir(com[1]) != 0))
+{
+free(com);
+perror("cd");
+}
+}
+}
