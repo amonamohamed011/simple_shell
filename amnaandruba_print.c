c04b6cac8959377f45c4ed5f4cb14446b199f066
amonamohamed011 <amonamohamed011@gmail.com>
2023-10-18 08:19:08 -0700

    amnaandruba_print.c

--- amnaandruba_print.c
+++ amnaandruba_print.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 /**
- * amnaandruba_print - function
- * @letter:the string to be printed
- * Return:This function doesnot return any value
+ * amnaandruba_print - this is a function.
+ * @letter:the string to be printed.
+ * Return:This function does not return any value.
  */
 void amnaandruba_print(char *letter)
 {
-	write(1, letter, strlen(letter));
+write(1, letter, strlen(letter));
 }
